744e5e4162408d091bdb5d2842c3807d021349bb
Zach David <112095798+zxaylex@users.noreply.github.com>
2025-07-25 17:53:45 +0000

    configured devcontainer

--- .devcontainer/postInstall.sh
+++ .devcontainer/postInstall.sh
@@ -1,16 +1,5 @@
 clear
 echo 'Welcome to LikAI, an AI-driven biosecurity coach designed for small and medium-sized shrimp farmers 🚀'
-echo -e "To start, run the command:\n\n npm run deploy:local\n\n to deploy the project in a local DFX network."
+echo -e "To start, please run this codespace in a Visual Studio Code Desktop Application."
 
-clear
-echo 'Welcome to LikAI, an AI-driven biosecurity coach designed for small and medium-sized shrimp farmers 🚀'
-
-echo -e "These are the frontend and backend urls for the local dfx network"
-if [ -f ".devcontainer/DEPLOY_URLS.txt" ]; then
-  echo -e "\nProject URLs:"
-  cat .devcontainer/DEPLOY_URLS.txt
-else
-  echo -e "\nURLs file not found. Did deployment succeed?"
-fi
-
-echo -e "You can visit the links as you wish!"
+echo -e "run the command:\n\nnpm run deploy:local\n\nto deploy the project in a local DFX network."
