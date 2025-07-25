8ee3703efd2762f288a28dc2ddabb6a4a426fc41
Zach David <112095798+zxaylex@users.noreply.github.com>
2025-07-26 01:57:38 +0800

    Update postInstall.sh

--- .devcontainer/postInstall.sh
+++ .devcontainer/postInstall.sh
@@ -1,5 +1,5 @@
 clear
 echo 'Welcome to LikAI, an AI-driven biosecurity coach designed for small and medium-sized shrimp farmers 🚀'
-echo -e "To start, please run this codespace in a Visual Studio Code Desktop Application."
+echo -e "To start, please run this codespace in a Visual Studio Code Desktop Application. You may go the Manu > Open in VS Code Desktop"
 
 echo -e "run the command:\n\nnpm run deploy:local\n\nto deploy the project in a local DFX network."
