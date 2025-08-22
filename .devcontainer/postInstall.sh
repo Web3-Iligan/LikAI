5bf581aac57de7ee861d4351224818d35ad1d08d
Zach <112095798+zxaylex@users.noreply.github.com>
2025-08-22 18:44:51 +0000

    new setup

--- .devcontainer/postInstall.sh
+++
@@ -1,6 +0,0 @@
-clear
-echo 'Welcome to LikAI, an AI-driven biosecurity coach designed for small and medium-sized shrimp farmers 🚀'
-echo -e "To start, the local dfx links are only accessible through forwarded ports. The links will only work inside the VS Code Desktop Application"
-echo -e "if you are not running the codespace, you may go the Manu > Open in VS Code Desktop"
-
-echo -e "run the command:\n\nnpm run deploy:local\n\nto deploy the project in a local DFX network."
